756e2ce45b25b18e8cfedab5cadaba9a153078fd
Anne <patesdegeek@gmail.com>
2020-04-14 21:10:37 +0200

    if imbriques

--- src/fr/abp/cours/Main.java
+++ src/fr/abp/cours/Main.java
@@ -20,6 +20,27 @@ public class Main {
         b = lectureClavier.nextDouble();
         System.out.println("Entrer la valeur de c :");
         c = lectureClavier.nextDouble();
-        
+
+        if (a == 0) {
+            if (b == 0) {
+                if (c == 0) {
+                    System.out.println("Tout réel est solution");
+                } else {
+                    System.out.println("Il n'existe pas de solution");
+                }
+            } else {
+                x = -c / b;
+                System.out.println("La solution est " + x);
+            }
+        } else {
+            delta = b * b - 4 * a * c;
+            if (delta < 0) {
+                System.out.println("La solution n'existe pas dans les réels");
+            } else {
+                x1 = (-b + Math.sqrt(delta)) / (2 * a);
+                x2 = (-b - Math.sqrt(delta)) / (2 * a);
+                System.out.println("Il y a deux solutions : " + x1 + " et " + x2);
+            }
+        }
     }
 }
