5d9577b92acfefde3d80c379f6a5075dc4195012
Anne <patesdegeek@gmail.com>
2020-04-14 20:57:18 +0200

    declaration des variables

--- src/fr/abp/cours/Main.java
+++ src/fr/abp/cours/Main.java
@@ -1,8 +1,25 @@
 package fr.abp.cours;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        double a;
+        double b;
+        double c;
+        double delta;
+        double x;
+        double x1;
+        double x2;
+
+        Scanner lectureClavier = new Scanner(System.in);
+        System.out.println("Entrer la valeur de a :");
+        a = lectureClavier.nextDouble();
+        System.out.println("Entrer la valeur de b :");
+        b = lectureClavier.nextDouble();
+        System.out.println("Entrer la valeur de c :");
+        c = lectureClavier.nextDouble();
+        
     }
 }
